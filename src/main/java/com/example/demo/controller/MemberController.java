57acc35239470b8b9e7926ee607cd1f057cd5b47
Huiyeon Kim <hing9u.k@gmail.com>
2022-07-01 18:02:25 +0900

    0701 updated!

--- src/main/java/com/example/demo/controller/MemberController.java
+++ src/main/java/com/example/demo/controller/MemberController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.example.demo.service.MemberService;
 
@@ -23,6 +24,11 @@ public class MemberController {
 	public MemberController(MemberService memberService) {
 		
 		this.memberService = memberService;
+	}
+	
+	@GetMapping("/members/new")
+	public String createForm() {
+		return "members/createMemberForm";
 	}
 	
 }
